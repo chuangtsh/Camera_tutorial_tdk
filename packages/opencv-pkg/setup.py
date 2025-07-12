c03e18c1709752cd3ec8b22262cdb8c98738b8b2
CYHsiang <seanchen1117@gmail.com>
2025-07-12 22:28:17 +0800

    Initializing Workspace

---
+++ packages/opencv-pkg/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+package_name = 'opencv_ros'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(include=['opencv_ros', 'opencv_ros.*']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    entry_points={
+        'console_scripts': [
+            'example = opencv_ros.example:main',  # Example entry point
+        ],
+    },
+    install_requires=['setuptools', 'opencv-python', 'cv_bridge', 'rclpy', 'numpy'],
+    zip_safe=True,
+    maintainer='CYH',
+    description='TDK camera opencv example',
+)
